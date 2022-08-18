6a09dc2ecd0d9a429e60ab9c5b6ea43689bf0c42
allanstar-byte <allanmutisya263@gmail.com>
2022-08-18 21:06:06 +0300

    Book

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+    def (title, author, page_count, genre)
+        @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre        
+    end
+end
 
-
+reads = Book.new("And Then There Were None","Agatha Christie", 272, "Mystery")
+puts reads.title
+puts reads.author
+puts reads.page_count
+puts reads.genre
