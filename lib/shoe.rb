5cad7bdc6df808666e43e1e8e9c198024d35f2af
allanstar-byte <allanmutisya263@gmail.com>
2022-08-18 21:18:59 +0300

    shoes

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
 # Make your shoe class here!
+
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+    def initialize(brand, color, size, material, condition)
+       @brand = brand
+       @color = color
+       @size = size
+       @material = material
+       @condition = condition
+    end
+end
+
+shoes = Shoe.new('Nike', 'red',8, 'lather', 'tattered')
+puts shoes.brand 
